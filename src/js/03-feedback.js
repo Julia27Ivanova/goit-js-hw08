5cc68eaaa5b229db0b71ee354422528f768cec76
Julia_Ivanova <109332548+Julia27Ivanova@users.noreply.github.com>
2023-04-09 00:13:46 +0300

    commit

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,6 +1,6 @@
 import throttle from 'lodash.throttle';
 
-const form = document.querySelector('.feedback.form');
+const form = document.querySelector('.feedback-form');
 
 const LOCALSTORAGE_KEY = 'feedback-form-state';
 
